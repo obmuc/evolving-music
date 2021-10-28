b48a138474896845841bcdf7289fe956a3083d55
obmuc <themightyobmuc@gmail.com>
2021-10-28 06:41:07 -0500

    continuing to build out the review page.

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -13,4 +14,19 @@ def home():
 
 @app.route("/review", methods = ['POST', 'GET'])
 def review():
-    print(request.form)
+    # TODO: configure midi files to be served by the web server and send a URL instead of a file path to the template.
+
+    seed_file = request.form.get('seed_file')
+
+    current_directory = os.getcwd()
+    midi_file_directory = f"{current_directory}/midi_files"
+
+    for dirpath, dirnames, files in os.walk(midi_file_directory):
+        for found_name in files:
+            if found_name == seed_file:
+                seed_file_with_path = os.path.join(dirpath, found_name)
+
+    return render_template(
+        'review.html',
+        seed_file_with_path = seed_file_with_path
+    )
